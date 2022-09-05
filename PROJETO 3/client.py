5c8d3927a2bdc5280a5e02dd9c0dbb9870888ca8
ariel leventhal <arielleventhal@gmail.com>
2022-09-05 20:16:08 -0300

    Co-authored-by: betomansur <betomansur@users.noreply.github.com>

---
+++ PROJETO 3/client.py
@@ -0,0 +1,121 @@
+# Camada Física da Computação
+#Carareto
+#11/08/2020
+#Aplicação
+####################################################
+
+
+#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
+#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
+
+
+from enlace import *
+import time
+import numpy as np
+from utils import *
+import sys
+# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM5"                  # Windows(variacao de)
+
+
+def main(com1:enlace):
+    #declaramos um objeto do tipo enlace com o nome "com". Essa é a camada inferior à aplicação. Observe que um parametro
+    #para declarar esse objeto é o nome da porta.
+    
+    
+    # Ativa comunicacao. Inicia os threads e a comunicação seiral 
+    com1.enable()
+    getTimedData = genDataRetrieval(com1)
+    #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
+    print("----------------------------------------------------------")
+    print("Comunicação aberta com sucesso! Vamos ao resto do projeto!")
+    print("----------------------------------------------------------")
+
+    #Envia e recebe byte de sacrifcio
+    print("enviando byte de sacrificio")
+    time.sleep(.2)
+    com1.sendData(b'00')
+    time.sleep(1)
+    print("aguardando byte de sacrificio")
+
+    com1.getData(1)
+    time.sleep(.1)
+
+    com1.rx.clearBuffer()
+    time.sleep(.2)
+
+    #le arquivo e o separa em pedacos que cabem no payload
+    imageR = "./img/wpp.jpg"
+    with open(imageR, "rb") as image: 
+        data = image.read()  #https://stackoverflow.com/questions/22351254/python-script-to-convert-image-into-byte-array
+    txBuffer = chunks(data,114)
+
+    #Faz handhsake
+    print("enviando handshale")
+    handshakePacket = buildPacket(p_type=PacketType.HANDSHAKE)
+    com1.sendData(handshakePacket);
+    received_answer = False
+    while not received_answer:
+        try:
+            print(com1.rx.getBufferLen())
+            verify_packet(getTimedData(14)[0],with_type=PacketType.ACK_HS)
+            print(com1.rx.getBufferLen())
+            received_answer = True
+            print("ACK received. Starting data transmission.")
+        except ComTimeoutError:
+            if input("Timeout. No response from server. Do you want to try again?(Y/n)") == "n":
+                raise Exception("Terminated due to inactive server")
+    totalPackages = len(data)//114
+    if len(data)%114!=0: totalPackages+=1
+    packetCount = 1
+    for imageChunk in txBuffer:
+        gotAck = False
+        while not gotAck:
+            try:
+                packet = buildPacket(payload=imageChunk,n_packet=packetCount,t_packets=totalPackages)
+                print(packet[:11])
+                com1.sendData(packet)
+                answer = getTimedData(14)
+                verify_packet(answer,with_type=PacketType.ACK_DATA)
+                gotAck = True
+                packetCount+=1
+            except ComTimeoutError:
+                print("Timeout awaiting for server. Trying again.")
+            except InvalidPacket as error:
+                print("Invalid answer from server. Trying again.")
+                print(error)
+
+    print("finished sending data")
+
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com1.disable()
+        
+
+        
+        
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    try:
+        com1 = enlace(serialName)
+        main(com1)
+    except KeyboardInterrupt:
+        com1.disable()
+        sys.exit()
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        com1.disable()
+        sys.exit()
+        
---
+++ PROJETO 3/server.py
@@ -0,0 +1,108 @@
+# Camada Física da Computação
+#Carareto
+#11/08/2020
+#Aplicação
+####################################################
+
+
+#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
+#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
+
+
+import sys
+from urllib.request import parse_keqv_list
+from enlace import *
+import time
+import numpy as np
+from utils import *
+# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM6"                  # Windows(variacao de)
+
+
+def main(com1:enlace):
+
+    #declaramos um objeto do tipo enlace com o nome "com". Essa é a camada inferior à aplicação. Observe que um parametro
+    #para declarar esse objeto é o nome da porta.
+
+
+
+    # Ativa comunicacao. Inicia os threads e a comunicação seiral 
+    com1.enable()
+    #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
+    print("----------------------------------------------------------")
+    print("Comunicação aberta com sucesso! Vamos ao resto do projeto!")
+    print("----------------------------------------------------------")
+
+
+
+    print("esperando 1 byte de sacrifício")
+    rxBuffer, nRx = com1.getData(1)
+    time.sleep(.1)
+
+    com1.rx.clearBuffer()
+    time.sleep(.2)
+    com1.sendData(b'00')
+    time.sleep(1)
+    getTimedData = genDataRetrieval(com1)
+
+    handshake,_ = com1.getData(14)
+    verify_packet(handshake, with_type=PacketType.HANDSHAKE)
+    print("received handshake")
+    time.sleep(.1)
+    com1.sendData(buildPacket(p_type=PacketType.ACK_HS))
+    com1.disable()
+    packetCount = 1
+    totalPackets = None
+    rxBuffer = bytearray()
+    while packetCount != totalPackets:
+        try:
+            head = getTimedData(10)
+            payloadSize = head[1]
+            packetCountRec = int.from_bytes(head[3:7],byteorder="big")
+            totalPacketsRec = int.from_bytes(head[7:],byteorder="big")
+            print(totalPacketsRec)
+            if packetCountRec!=packetCount:
+                raise InvalidPacket("wrong packet number")
+            if totalPackets == None:
+                totalPackets=totalPacketsRec
+            elif totalPackets != totalPacketsRec:
+                InvalidPacket("Wrong total packets")
+            payload = getTimedData(payloadSize)
+            eop = getTimedData(4)
+            packet = head+payload+eop
+            verify_packet(packet, with_type=PacketType.DATA)
+            com1.sendData(PacketType.ACK_DATA)
+            packetCount+=1
+            rxBuffer+=payload
+        except (ComTimeoutError,InvalidPacket) as error:
+            print(error)
+            com1.sendData(buildPacket(p_type=PacketType.NO_ACK_DATA))
+            time.sleep(.1)
+            com1.rx.clearBuffer()
+            time.sleep(.1)
+        with open(".img/copiaDevolvida.jpg",'wb') as f:
+            f.write(rxBuffer)
+        com1.disable()
+
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    try:
+        com1 = enlace(serialName)
+        main(com1)
+    except KeyboardInterrupt:
+        com1.disable()
+        sys.exit()
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        com1.disable()
+        sys.exit()
+        
